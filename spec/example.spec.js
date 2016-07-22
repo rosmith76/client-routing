6c95fb88b7a840ae94f509611afdc1bf807abc05
Jeffrey Horn <jeffh@generalassemb.ly>
2016-07-22 11:31:31 -0400

    Initial commit

---
+++ spec/example.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const example = require('../assets/scripts/router');
+
+describe('Example', function () {
+  it('is true', function () {
+    expect(example).toBe(true);
+  });
+});
