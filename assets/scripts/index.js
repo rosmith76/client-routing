23e96e597252d4e488f7a49b8c83871f0e5551f7
berziiii <brian@offpistedesigns.com>
2016-10-28 07:52:49 -0400

    Update from jeffh working repo
    - Update various files to match jeff's repo on his gh account
    - pull over starter code to ensure correct starting place for
    the developers

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,15 +1,7 @@
 'use strict';
 
-const router = require('./router/index');
+// user require with a reference to bundle the file and use it in this file
+// var example = require('./example');
 
-// useMiddleware should only ever be called once
-// see: http://router5.github.io/docs/middleware.html
-router.useMiddleware(require('./router/dom').transition);
-
-const routerEvents = require('./router/events');
-
-$(() => {
-  router.start();
-
-  routerEvents.addHandlers();
-});
+// use require without a reference to ensure a file is bundled
+require('./router');
